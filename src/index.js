2331c9f638cd6b04c3bd3d3206b87bb7555cb951
Your Name <fernandez.Leovanny@gmail.com>
2018-08-29 16:14:46 -0400

    recent changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import {HashRouter, Route, Link} from 'react-router-dom'
+import axios from 'axios'
 
 class Main extends Component {
   render() {
     return(
-      <div>
-      <Form />
-      </div>
+      <HashRouter>
+        <Route path='/user/entry' component = {Form} />
+      </HashRouter>
     )
   }
 }
@@ -39,7 +41,8 @@ class Form extends Component {
   }
   handleSubmit(event) {
     event.preventDefault();
-    console.log(this.state)
+
+
     this.setState({
       name:'',
       city:'',
