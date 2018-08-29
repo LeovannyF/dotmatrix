e0f563c856e6117d3dc4ad9973bb30dac5975930
Your Name <fernandez.Leovanny@gmail.com>
2018-08-29 15:39:40 -0400

    duo work setup

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ const bodyParser = require('body-parser');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/form', (req, res, next) => {  // where users will input data
+app.get('/api/form', (req, res, next) => {  // where users will input data
   res.send('FORMS FOR DAYS')
 });
 
 app.get('/', (req, res, next) => { //post that will eventually grab the data that I am trying to post
  res.sendFile(path.join(__dirname, 'public/index.html'))
 });
-app.post('/form', (req, res, next) => {
+
+app.post('/api/form', (req, res, next) => {
 
 })
-
 
 app.listen(port, ()=>{
   console.log(`I am listening on port, ${port}`);
