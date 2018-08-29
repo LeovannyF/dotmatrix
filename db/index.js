7a179fbb2715386ccb36531852c2f133a2821198
Your Name <fernandez.Leovanny@gmail.com>
2018-08-29 14:42:21 -0400

    cleaned out the data models to simplify

--- db/index.js
+++ db/index.js
@@ -1,10 +1,6 @@
 Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/dotMatrix');
-const {testCity, testAuthor, testMessages} = require('./seed');
-
-const Message = db.define('message', {
-  note: Sequelize.STRING
-});
+const {testCity, testAuthor} = require('./seed');
 
 const Author = db.define('author', {
   name: Sequelize.STRING,
@@ -13,19 +9,13 @@ const Author = db.define('author', {
 });
 
 const seed = async () =>{
-  const [message1, message2, message3] = await Promise.all(testMessages.map(message => {
-    return Message.create({note: message});
-  }))
   const [Leo, Eve, Claudia] = await Promise.all(testAuthor.map(author => {
     return Author.create({name: author});
   }))
 }
 
-Message.belongsTo(Author, {as:'Owner'});
-
 module.exports = {
   db,
   Author,
-  Message,
   seed
 }
--- db/seed.js
+++ db/seed.js
@@ -1,11 +1,5 @@
-const testMessages = ['Save Us', 'We Will Burn', 'Love All'];
-
 const testAuthor = ['Leo', 'Eve', 'Claudia'];
 
-const testCity = ['Santo Domingo', 'New York City', 'London'];
-
 module.exports = {
-  testCity,
   testAuthor,
-  testMessages
 };
