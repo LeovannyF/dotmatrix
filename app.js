14a393f30507edd5dc50a7cae78664213173dd50
Sam Wellander <samwellander@gmail.com>
2018-09-13 08:57:04 -0400

    my editor auto-formatted

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const {db, seed, Entry} = require('./db/index');
+const { db, seed, Entry } = require('./db/index');
 const port = process.env.PORT || 3000
 const path = require('path')
 const bodyParser = require('body-parser');
@@ -15,32 +15,34 @@ app.get('/api/entry', (req, res, next) => {
   console.log('hit')
   Entry.findAll({
     limit: 1,
-    order:[
+    order: [
       ['createdAt', 'DESC']
     ]
   })
-  .then(response => res.send(response))
-  .catch(next);
+    .then(response => res.send(response))
+    .catch(next);
 })
 
 app.get('/', (req, res, next) => {
- res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 });
 
 app.post('/api/user/entry', (req, res, next) => {
-  Entry.create({name: req.body.name,
-                city: req.body.city,
-                content: req.body.content})
-  .then(res.sendStatus(200))
-  .catch(next);
+  Entry.create({
+    name: req.body.name,
+    city: req.body.city,
+    content: req.body.content
+  })
+    .then(res.sendStatus(200))
+    .catch(next);
 })
 
-const server = app.listen(port, ()=>{
+const server = app.listen(port, () => {
   console.log(`I am listening on port, ${port}`);
 });
 
-const init = async() => {
-  await db.sync({force:true})
+const init = async () => {
+  await db.sync({ force: true })
   await seed();
 }
 
